6ce59ac5e377304780b8658b1192289552808994
ChaabaniHajar <76130689+ChaabaniHajar@users.noreply.github.com>
2023-01-14 23:49:59 +0100

    login system

---
+++ src/main/java/com/gestion/absence/security/MvcConfig.java
@@ -0,0 +1,17 @@
+package com.gestion.absence.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("login");
+        registry.addViewController("/profile_enseignant").setViewName("profile_enseignant");
+        registry.addViewController("/admin").setViewName("admin");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
---
+++ src/main/java/com/gestion/absence/security/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.gestion.absence.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    public WebSecurityConfig(AuthenticationSuccessHandler authenticationSuccessHandler) {
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers( "/css/**", "/images/**", "/favicon.ico").permitAll()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .successHandler(authenticationSuccessHandler)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and().csrf().disable(); // we'll enable this in a later blog post
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("enseignant").password("{noop}pass").roles("ENSEIGNANT")
+                
+                .and()
+                .withUser("admin").password("{noop}pass").roles("ADMIN");
+    }
+}
